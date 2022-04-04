337ae5a4cbc9e7034e2095b8af3c4f883380928c
DhruvZopsmart <dhruv.batra@zopsmart.com>
2022-04-04 10:49:21 +0530

    assignment_day8

--- assignment_day8/bulding_server.js
+++ assignment_day8/bulding_server.js
@@ -1,31 +1,50 @@
 const http = require('http');
 require('dotenv').config();
 
-const port = process.env.PORT ;
+const port = process.env.PORT || 3000;
 const fs = require('fs');
 
 const server = http.createServer((req,res)=>{
-    console.log(typeof(req.method));
+    console.log(typeof(req.method), req.url);
 
     // console.log(req);
    
      if(req.method == 'GET' &&  req.url =='/method'){
         // console.log('get');
-        fs.readFile('index.html', 'utf8' , (err, data)=>{
+        fs.readFile('./static/index.html', 'utf8' , (err, data)=>{
             if(err){
-                res.statusCode(500);
+                res.statusCode =500;
                 res.setHeader('Content-Type' , 'text/html')
+                res.write('<h1> Internal server error</h1>')
+                res.end();
+            }
+            
+            else {
+                res.statusCode= 200;
+                res.setHeader('Conte1nt-Type','text/html');
+                console.log('html');
+
+                res.end(data);
+            }
+        })
+    }
+    else if ("./index.css".includes(req.url)){
+        fs.readFile('./static/index.css', 'utf8' , (err, data)=>{
+            if(err){
+                res.statusCode =500;
+                // res.setHeader('Content-Type' , 'text/html')
                 res.end('<h1> Internal server error</h1>')
             }
             
             else {
                 res.statusCode= 200;
-                res.setHeader('Content-Type','text/html');
-                res.end(data);
+                // res.setHeader('Content-Type','text/html');
+                res.write(data);
+                console.log('css applied')
+                res.end();
             }
         })
     }
-
     else if(req.method == 'POST' && req.url =='/echo')
     {
         let body='';
